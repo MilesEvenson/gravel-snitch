324bd1c5efb5c9e233ba32abab157db5cece7f1c
Miles Evenson <miles.evenson@gmail.com>
2021-10-17 21:41:43 -0400

    [meta] - tweak css path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -34,7 +34,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: '/gravel_snitch/'
   },
   plugins: [
     new HtmlWebPackPlugin({
